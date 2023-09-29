85aaae010e2c71eedcf8acff570a32dbe72b9d56
trong.m.bui <trong.m.bui@pkh.vn>
2023-09-29 15:25:13 +0700

    feat: RabbitConsumer JSON

--- src/main/java/com/trong/rabbitmq/consumer/RabbitConsumer.java
+++ src/main/java/com/trong/rabbitmq/consumer/RabbitConsumer.java
@@ -1,17 +1,28 @@
 package com.trong.rabbitmq.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trong.rabbitmq.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class RabbitConsumer {
 
     private static final Logger logger = LoggerFactory.getLogger(RabbitConsumer.class);
 
-//    @RabbitListener(queues = {"${rabbit.config.queueName}"})
-//    public void consume(String message) {
-//        logger.info(String.format("Received message -> %s", message));
-//    }
+    @RabbitListener(queues = {"${rabbit.config.queueName}"})
+    public void consume(String message) {
+        logger.info(String.format("Received message -> %s", message));
+    }
+
+    @RabbitListener(queues = {"${rabbit.config.queueNameJSON}"})
+    public void consume(Map<String, Object> user) throws JsonProcessingException {
+        ObjectMapper obj = new ObjectMapper();
+        logger.info(String.format("Received message JSON -> %s", obj.writeValueAsString(user)));
+    }
 }
--- src/main/java/com/trong/rabbitmq/model/User.java
+++ src/main/java/com/trong/rabbitmq/model/User.java
@@ -6,7 +6,6 @@ import java.util.Date;
 
 @Data
 public class User {
-
     private String name;
     private int age;
     private Date birthDay;
