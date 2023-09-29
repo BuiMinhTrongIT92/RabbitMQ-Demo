495432df8c27877b93e391483d4b1a0ef94a1acb
trong.m.bui <trong.m.bui@pkh.vn>
2023-09-29 13:26:16 +0700

    'initProject'

---
+++ src/main/java/com/trong/rabbitmq/config/RabbitConfig.java
@@ -0,0 +1,38 @@
+package com.trong.rabbitmq.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Value("rabbit.config.topicExchangeName")
+    private String topicExchangeName;
+
+    @Value("rabbit.config.queueName")
+    private String queueName;
+
+    @Value("rabbit.config.routingKey")
+    private String routingKey;
+
+    @Bean
+    public Queue queue(){
+        return new Queue(queueName);
+    }
+
+    @Bean
+    public TopicExchange topicExchange(){
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    public Binding binding(){
+        return BindingBuilder.bind(queue())
+                .to(topicExchange()).with(routingKey);
+    }
+}
---
+++ src/main/java/com/trong/rabbitmq/publisher/RabbitProducer.java
@@ -0,0 +1,29 @@
+package com.trong.rabbitmq.publisher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+
+public class RabbitProducer {
+
+    @Value("rabbit.config.topicExchangeName")
+    private String topicExchangeName;
+
+    @Value("rabbit.config.routingKey")
+    private String routingKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    private static final Logger logger = LoggerFactory.getLogger(RabbitProducer.class);
+
+    public RabbitProducer(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    //Using send message from Producer
+    public void sendMessage(String message){
+        logger.info(String.format("Message sent -> %s", message));
+        rabbitTemplate.convertAndSend(topicExchangeName,routingKey, message);
+    }
+}
